79b4ee5e6444d247ad5d44f42d0bad3a8730c173
erastov-alex <erastov.alex@gmail.com>
2023-10-27 17:14:08 +0300

    upd convert

--- src/convert.py
+++ src/convert.py
@@ -3,7 +3,7 @@ import os
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
+from supervisely.io.fs import get_file_name, file_exists
 import shutil
 
 from tqdm import tqdm
@@ -69,17 +69,79 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    
+    bboxes_path = os.path.join("EALPR-master","EALPR- LP characters dataset","Characters Labeling")
+    images_path = os.path.join("EALPR-master","EALPR- Plates dataset")
+    classes_path = os.path.join(bboxes_path, "classes.txt")
+    bbox_ext = ".txt"
+    ds_name = "ds"
+    batch_size = 30
+    bad_data = ['EALPR-master/EALPR- Plates dataset/1088_license_plate_1.png']
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        bbox_path = os.path.join(bboxes_path, get_file_name(image_path) + bbox_ext)
 
-    # return project
+        if file_exists(bbox_path):
+            with open(bbox_path) as f:
+                content = f.read().split("\n")
+
+                for curr_data in content:
+                    if len(curr_data) != 0:
+                        curr_data = list(map(float, curr_data.split(" ")))
+                        obj_class = id2class[curr_data[0]]
+                        left = int((curr_data[1] - curr_data[3] / 2) * img_wight)
+                        right = int((curr_data[1] + curr_data[3] / 2) * img_wight)
+                        top = int((curr_data[2] - curr_data[4] / 2) * img_height)
+                        bottom = int((curr_data[2] + curr_data[4] / 2) * img_height)
+                        rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                        label = sly.Label(rectangle, obj_class)
+                        labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    id2class = {}
+    with open(classes_path) as f:
+                content = f.read().split("\n")
+                for id, curr_data in enumerate(content):
+                    id2class[id] = sly.ObjClass(curr_data, sly.Rectangle)
 
 
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=list(id2class.values()))
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    all_images = [im_name for im_name in os.listdir(images_path) if im_name != "2018.jpg"]
+    all_images = [im_name for im_name in all_images if im_name.lower().endswith(".png")]
+
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(all_images))
+    errors = []
+    for img_names_batch in sly.batched(all_images, batch_size=batch_size):
+        img_pathes_batch = [os.path.join(images_path, im_name) for im_name in img_names_batch]
+        try:
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+        except Exception:
+            for name, path in zip(img_names_batch, img_pathes_batch):
+                try:
+                    info = api.image.upload_path(dataset.id, name, path)
+
+                    ann = create_ann(path)
+                    api.annotation.upload_ann(info.id, ann)
+                except Exception:
+                    errors.append(path)
+    
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
